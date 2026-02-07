fd4596bee6c4b2ab55151655ef48e9c4887d60a7
michaelzamo <39522968+michaelzamo@users.noreply.github.com>
2026-02-07 06:49:12 +0000

    Bugfix: bad import from sqlAlchemy

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,15 +1,15 @@
-from sqlalchemy import create_url
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
 
-# Pour le moment, on utilise SQLite pour simplifier le démarrage dans Codespaces
-# On pourra passer à PostgreSQL plus tard sans changer le code
+# Utilisation de SQLite pour le développement local
 SQLALCHEMY_DATABASE_URL = "sqlite:///./japanese_app.db"
 
+# L'argument connect_args={"check_same_thread": False} est spécifique à SQLite
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
